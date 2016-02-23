10ab1715533e8b0f6de23d64361367d92e81c052
interactiveAli <ali@interactiveali.com>
2016-02-23 10:32:56 -0500

    Update start.js

--- start.js
+++ start.js
@@ -5,8 +5,6 @@
 // $ npm install cookie-parser --save
 // $ npm install multer --save
 // $ npm install archiver --save
-// $ npm install requirejs --save
-// $ npm install jquery --save
 // Navigate to tinythumbs directory and start app...
 // tinythumbs $ node start.js
 
